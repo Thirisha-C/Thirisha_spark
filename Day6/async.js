6d8c3d54b8cc1611de0f0c8f501789868581efdd
Thirisha-C <thirisha@sparkouttech.com>
2026-03-06 19:01:02 +0530

    adding day6 files

--- Day6/async.js
+++ Day6/async.js
@@ -6,3 +6,41 @@ async function fn(){
 console.log(fn());
 
 fn().then((msg) => console.log(msg));
+
+// async await
+
+function placeOrder() {
+    return new Promise (resolve =>
+        {
+        setTimeout(()=>  {
+            console.log("Order placed");
+            resolve();
+        },1000);
+    });
+}
+
+function prepareFood() {
+    return new Promise(resolve => {
+        setTimeout(()=> {
+            console.log("Food prepared");
+            resolve();
+        },2000);
+    });
+}
+
+function deliverFood() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log("Food delivered");
+            resolve();
+        },1000);
+    });
+}
+
+async function order() {
+    await placeOrder();
+    await prepareFood();
+    await deliverFood();
+}
+
+order();
